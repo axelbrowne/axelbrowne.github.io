60fc2dfead4793f0a760386f8f8aa0cb8fc99c51
damianbrowne <damianabrowne@gmail.com>
2022-01-03 20:05:59 -0800

    From here on out, I'm fucking things up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css'
+import Main from './components/pages/Main'
 import Navbar from './components/Navbar'
 import Projects from './components/pages/Projects'
 import About from './components/pages/About'
+import Home from './components/pages/Home'
 
 function App() {
   return (
   <Router basename='/'>
     <div className="page-container">
       <div className="content-wrap">
-        <Navbar/>
+        <Main/>
+        {/* <Navbar/>
+        <Home/>
         <Switch>
           <Route path='/' exact component={About} />
           <Route path='/projects' exact component={Projects} />
           <Route path='/about' exact component={About} />
-        </Switch>
+        </Switch> */}
       </div>
     </div>
   </Router>
---
+++ src/components/pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import About from '../../components/pages/About'
+import Projects from '../../components/pages/Projects'
+
+function Main() {
+    return (
+        <>
+            <Navbar />
+            <About/>
+            <Projects/>
+        </>
+    )
+}
+
+export default Main;
